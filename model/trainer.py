1236029e8499024026668643d71c61653ee0cafb
春草 <chuncao.jj@alibaba-inc.com>
2025-01-07 12:56:30 +0800

    scheduler module

--- model/trainer.py
+++ model/trainer.py
@@ -29,24 +29,6 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from model.gpt import GPTModel, CONFIG_OPERATION, ModelWrapper
 from module.scheduler import LinearWarmupLinearDecayScheduler
 
-"""
-class LinearWarmupLinearDecayScheduler(torch.optim.lr_scheduler._LRScheduler):
-    def __init__(self, optimizer, warmup_steps, total_steps, last_epoch=-1):
-        self.warmup_steps = warmup_steps
-        self.total_steps = total_steps
-        super().__init__(optimizer, last_epoch)
-
-    def get_lr(self):
-        step = self.last_epoch
-        if step < self.warmup_steps:
-            return [base_lr * (step / self.warmup_steps) for base_lr in self.base_lrs]
-        else:
-            return [
-                base_lr * max(0.0, (1 - (step - self.warmup_steps) / (self.total_steps - self.warmup_steps)))
-                for base_lr in self.base_lrs
-            ]
-"""
-
 class Trainer():
     """
     Trainer class for training and evaluating a GPTModel.
