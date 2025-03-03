1ba3473dd759d3346aba93641e4cd9e4acbd70e4
春草 <chuncao.jj@alibaba-inc.com>
2025-03-03 17:25:27 +0800

    fix bugs

---
+++ sft/hh_instruction.py
@@ -0,0 +1,91 @@
+import os
+import sys
+import json
+import torch
+from dataset.dataset import InstructionDataset, ABCDataLoader
+from tokenizer.tokenizer import SPTokenizer
+from model.gpt import GPTModel
+from sft.instruction import InstructionFinetune
+
+
+torch.manual_seed(123)
+num_workers = 0
+batch_size = 8
+tokenizer = SPTokenizer("./data/ChatGLMTokenizer/tokenizer.model")
+ignore_index = -200
+#train_file = "./data/finetune/train-instruction-data.json"
+#val_file = "./data/finetune/val-instruction-data.json"
+
+#train_file = "../corpus/BAAI_COIG/human_value_alignment_instructions_part1.json"
+train_file = "./data/finetune/huanhuan.json"
+val_file = "./data/finetune/val-huanhuan.json"
+
+train_dataset = InstructionDataset(
+    train_file, 
+    tokenizer,
+    ignore_index=ignore_index
+)
+train_loader = ABCDataLoader(
+    train_dataset,
+    batch_size=batch_size,
+    shuffle=True,
+    drop_last=True,
+    num_workers=num_workers,
+)
+
+val_dataset = InstructionDataset(
+    val_file,
+    tokenizer,
+    ignore_index=ignore_index
+)
+val_loader = ABCDataLoader(
+    val_dataset,
+    batch_size=batch_size,
+    shuffle=False,
+    drop_last=False,
+    num_workers=num_workers,
+)
+
+base_model = "data/baike_steps_171000"
+model = GPTModel.from_pretrained(base_model)
+model.cfg["accumulation_steps"] = 1
+
+finetune = InstructionFinetune(model, tokenizer, max_length=128)
+finetune.ignore_index = ignore_index
+
+output_ckpt="./data/tmp/finetune/instruct"
+if os.path.isdir(output_ckpt):
+    finetune.load_lastest(output_ckpt)
+
+with open(val_file, "r", encoding="utf-8") as f:
+    items = json.load(f)
+
+"""
+finetune.train(
+    train_loader=train_loader, 
+    val_loader=val_loader,
+    max_length=512,
+    num_epochs=1,
+    eval_freq=100,
+    eval_iter=1,
+    sample_iter=10000,
+    dump_path=output_ckpt,
+    start_context=InstructionDataset.format_input(items[0], with_output=True)
+)
+"""
+
+with open(train_file, "r", encoding="utf-8") as f:
+    items = json.load(f)
+
+for item in items:
+    data = InstructionDataset.format_input(item, with_output=False)
+    response_json = finetune.generate(
+        start_context=data,
+        temperature=0.8,
+        top_k=3,
+        max_length=512)
+    
+    print(f"Start Context:\n{response_json['Start_Context']}\n{'*' * 80}")
+    print(f"\nCorrect response:>>\n {item['output']}\n{'*' * 80}")
+    print(f"\nModel response:>>\n {response_json['Generate_Text']}\n{'-'*80}")
+
